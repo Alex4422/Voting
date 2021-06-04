feaa1233f52ac93e4d68344f89ac8aac284e2e6a
Alex4422 <alexandregautier82@gmail.com>
2021-06-04 18:24:52 +0200

    Code of the skeleton is OK

---
+++ Voting.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.6.11;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+/**
+ * @title Voting
+ * @dev Manages a voting system
+ */
+contract Voting {
+
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    uint public winningProposalID;
+
+    event VoterRegistered(address voterAddress);
+    event ProposalsRegistrationStarted();
+    event ProposalsRegistrationEnded();
+    event ProposalRegistered(uint proposalId);
+    event VotingSessionStarted();
+    event VotingSessionEnded();
+    event Voted (address voter, uint proposalId);
+    event VotesTallied();
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus
+    newStatus);
+
+    function Voting(){
+
+    }
+}
